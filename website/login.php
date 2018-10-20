0e3375735a7aec76823a284a8dc08e7d722a249e
lu juihung <raylu@10-249-237-45.wireless.oregonstate.edu>
2018-10-19 18:35:58 -0700

    ray update

--- website/login.php
+++ website/login.php
@@ -7,7 +7,7 @@ $link = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-        $url = "http://web.engr.oregonstate.edu/~lujui/index.html"; 
+        $url = "http://web.engr.oregonstate.edu/~lujui/index2.html"; 
         $account = $_POST["account"];
         $password = $_POST["password"];
 
@@ -19,7 +19,8 @@ if ($mysqli->connect_errno) {
         if (mysqli_num_rows($data1)==1 )
         {
 
-            header("Location: $url");
+            header("Location: $url?$account");
+
         }else{
         	print '<center>Login error</center>';
         	echo '<center><a href="index.html">return to main page</a><br></center>';
@@ -33,3 +34,5 @@ $mysqli->close();
     <?php if(!empty($aaa['errMsg'])) { echo $aaa['errMsg']; } ?>
 </div>
 <?php unset($aaa['errMsg']); ?>
+$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+        $new_url = str_replace('login.php', 'index.html', $url);
